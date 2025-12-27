506b4aa24088dc958ffcc90034d0614f6adc8c34
loc <lluu3790@gmail.com>
2025-12-27 00:48:56 -0600

    Lucas

--- src/components/ContactMeSection.jsx
+++ src/components/ContactMeSection.jsx
@@ -36,7 +36,7 @@ const LandingSection = () => {
         name: values.firstName,
         email: values.email,
         message: values.comment,
-        host: "Lewis Luu",
+        host: "Lucas Luu",
       };
       const sendE = await fetchApi(sendAPI);
       console.log(sendE);
